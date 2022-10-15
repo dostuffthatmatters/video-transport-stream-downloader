aa8805068b25e12c564ebffb23c771f952d7aa00
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-16 00:01:37 +0200

    Use subdirectory for raw data

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ with open(os.path.join(PROJECT_DIR, "config.json")) as f:
     CONFIG = json.load(f)
 
 DATA_DIR = os.path.join(PROJECT_DIR, "out")
+RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
 CHUNKLIST_URL = CONFIG["chunklist_url"]
 TITLE = CONFIG["title"]
-CHUNKLIST_PATH = os.path.join(DATA_DIR, f"{TITLE}.m3u8")
+CHUNKLIST_PATH = os.path.join(DATA_DIR, f"{TITLE}-chunklist.m3u8")
 BASE_URL = "/".join(CHUNKLIST_URL.split("/")[:-1])
 
 # TODO: Do not download a chunklist that is already cached
@@ -20,9 +21,9 @@ with open(CHUNKLIST_PATH) as f:
 
 file_pattern = re.compile("[^\n].*_\d+\.ts")
 files = re.findall(file_pattern, chunklist)
-missing_files = [f for f in files if not os.path.isfile(os.path.join(DATA_DIR, f))]
+missing_files = [f for f in files if not os.path.isfile(RAW_DATA_DIR, f)]
 file_urls = [f"{BASE_URL}/{f}" for f in missing_files]
-os.system("wget " + " ".join(file_urls) + f" -P {DATA_DIR}")
+os.system("wget " + " ".join(file_urls) + f" -P {RAW_DATA_DIR}")
 
 # TODO: cat streams into one all.ts
 # TODO: convert all.ts into mp4
