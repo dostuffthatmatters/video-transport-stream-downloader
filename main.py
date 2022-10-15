17649530112cfbbd46c44bfd9359a9270f95516c
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-16 00:34:58 +0200

    Remove temporary files

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import re
+import shutil
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,19 +21,20 @@ BASE_URL = "/".join(CHUNKLIST_URL.split("/")[:-1])
 os.system(f"curl {CHUNKLIST_URL} > {CHUNKLIST_PATH}")
 with open(CHUNKLIST_PATH) as f:
     chunklist = f.read()
+os.remove(CHUNKLIST_PATH)
 
-# download all missing ts files
+# download all ts files
 file_pattern = re.compile("[^\n].*_\d+\.ts")
 files = re.findall(file_pattern, chunklist)
-missing_files = [f for f in files if not os.path.isfile(os.path.join(RAW_DATA_DIR, f))]
-file_urls = [f"{BASE_URL}/{f}" for f in missing_files]
+file_urls = [f"{BASE_URL}/{f}" for f in files]
 if not os.path.isdir(RAW_DATA_DIR):
     os.mkdir(RAW_DATA_DIR)
 os.system("wget " + " ".join(file_urls) + f" -P {RAW_DATA_DIR}")
 
-# merge all ts files into one ts file
+# merge all ts files into one ts file + delete individual ts files
 raw_ts_files = [os.path.join(RAW_DATA_DIR, f) for f in files]
 os.system(f"cat " + " ".join(raw_ts_files) + f" > {MERGED_TS_PATH}")
+shutil.rmtree(RAW_DATA_DIR)
 
 # convert the merged ts file into an mp4 file + delete merged ts file
 if os.path.isfile(MERGED_MP4_PATH):
