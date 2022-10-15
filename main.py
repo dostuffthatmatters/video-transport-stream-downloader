b75270b022b7b978b14f971c21d44e956f1f421f
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-15 23:54:19 +0200

    Add todos

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import re
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
+DATA_DIR = os.path.join(PROJECT_DIR, "out")
 with open(os.path.join(PROJECT_DIR, "config.json")) as f:
     CONFIG = json.load(f)
     CHUNKLIST_URL = CONFIG["chunklist_url"]
     TITLE = CONFIG["title"]
 
-CHUNKLIST_PATH = os.path.join(PROJECT_DIR, "out", f"{TITLE}.m3u8")
+# TODO: Do not download a chunklist that is already cached
+CHUNKLIST_PATH = os.path.join(DATA_DIR, f"{TITLE}.m3u8")
 os.system(f"curl {CHUNKLIST_URL} > {CHUNKLIST_PATH}")
 with open(CHUNKLIST_PATH) as f:
     chunklist = f.read()
@@ -18,7 +19,11 @@ with open(CHUNKLIST_PATH) as f:
 file_pattern = re.compile("[^\n].*_\d+\.ts")
 files = re.findall(file_pattern, chunklist)
 
+# TODO: Do not download files that have already been cached
 BASE_URL = "/".join(CHUNKLIST_URL.split("/")[:-1])
 os.system(
     "wget " + " ".join([f"{BASE_URL}/{f}" for f in files]) + f" -P {PROJECT_DIR}/out"
 )
+
+# TODO: cat streams into one all.ts
+# TODO: convert all.ts into mp4
