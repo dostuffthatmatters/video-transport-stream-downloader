7c02c4ac4917e5dca27d28837de923a5b9312619
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-15 23:34:33 +0200

    Move parameters to config file

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import json
 import os
 
-SRC = "https://.../some.m3u8"
+
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-CHUNKLIST_PATH = os.path.join(PROJECT_DIR, "out", "chunklist.m3u8")
 
-os.system(f"curl {SRC} > {CHUNKLIST_PATH}")
+with open(os.path.join(PROJECT_DIR, "config.json")) as f:
+    CONFIG = json.load(f)
+    CHUNKLIST_URL = CONFIG["chunklist_url"]
+    TITLE = CONFIG["title"]
 
+CHUNKLIST_PATH = os.path.join(PROJECT_DIR, "out", f"{TITLE}.m3u8")
+os.system(f"curl {CHUNKLIST_URL} > {CHUNKLIST_PATH}")
 with open(CHUNKLIST_PATH) as f:
     chunklist = f.read()
