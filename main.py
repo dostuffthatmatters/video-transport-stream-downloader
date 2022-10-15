ded1c7f0c82c9ce9172af328a1e9f2a2064ee508
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-10-15 23:53:05 +0200

    Download *.ts files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -13,3 +14,11 @@ CHUNKLIST_PATH = os.path.join(PROJECT_DIR, "out", f"{TITLE}.m3u8")
 os.system(f"curl {CHUNKLIST_URL} > {CHUNKLIST_PATH}")
 with open(CHUNKLIST_PATH) as f:
     chunklist = f.read()
+
+file_pattern = re.compile("[^\n].*_\d+\.ts")
+files = re.findall(file_pattern, chunklist)
+
+BASE_URL = "/".join(CHUNKLIST_URL.split("/")[:-1])
+os.system(
+    "wget " + " ".join([f"{BASE_URL}/{f}" for f in files]) + f" -P {PROJECT_DIR}/out"
+)
